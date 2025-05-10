3980608c579e9242a85d3474a5a0ad6aceb75fda
jonzaba <jonznzabala111@gmail.com>
2025-05-10 17:19:04 +0200

    lista canales

--- src/main/java/com/example/backfroga/ApiController.java
+++ src/main/java/com/example/backfroga/ApiController.java
@@ -1,9 +1,16 @@
 package com.example.backfroga;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.Resources;
 
 @RestController
 @RequestMapping("/api")
@@ -12,5 +19,17 @@ public class ApiController {
     @GetMapping("/hello")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from the backend (JON call api)!");
+    }
+    
+    @GetMapping("/canales")
+    public ResponseEntity<String> getCanales() {
+    	try {
+            ClassPathResource resource = new ClassPathResource("static/lista.m3u");
+            InputStream inputStream = resource.getInputStream();
+            String content = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            return ResponseEntity.ok(content);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Error al leer el archivo: " + e.getMessage());
+        }
     }
 }
